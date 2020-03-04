da2e4b9c89a470fdcf5251a959b39d3ce50e50f9
jimmyliu1326 <jimmyliu1326@yahoo.com>
2020-03-03 20:29:27 -0800

    added --tmp

--- LT_main.sh
+++ LT_main.sh
@@ -7,6 +7,7 @@ Usage: $0
  -1       Input forward fastq
  -2       Input reverse fastq
  -o       specify output directory
+ -tmp     specify directory for storing temporary files
  
  Options:
  -t       Number of threads [Default: 1]
@@ -25,8 +26,7 @@ then
     exit
 fi
 
-opts=`getopt -o h1:2:o:t: -l qc,mlst,amr,help \
-      -- "$@"`
+opts=`getopt -o h1:2:o:t: -l tmp,help -- "$@"`
 if [ $? != 0 ] ; then echo "WARNING: Invalid arguements used, exiting"; usage; exit 1 ; fi
 eval set -- "$opts"
 
@@ -40,3 +40,16 @@ while true; do
     -h|--help) usage; exit ;;
   esac
 done
+
+fastp_exe() {
+  time=$(date +"%T")
+  echo "[$time] Read trimming using fastp"
+
+  source /opt/galaxy/tool_dependencies/_conda/bin/activate /home/$USER/.conda/envs/fastp
+  cleaned_fastq_1=/scratch/$USER/tmp/$(basename ${fastq_1%.*})_clean.fastq
+  cleaned_fastq_2=/scratch/$USER/tmp/$(basename ${fastq_2%.*})_clean.fastq
+  fastp -i $fastq_1 -I $fastq_2 \
+        -o $cleaned_fastq_1 \
+        -O $cleaned_fastq_2 \
+        -w $n_threads &> $OUT_DIR/log_files/fastp.log
+}
